66da786a52bd44adb16bb7d2437d4ffb16642217
evenchange4 <evenchange4@ubuntu.(none)>
2014-06-02 16:09:10 +0800

    run again

--- run.sh
+++ run.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
-
 group_number=$1
 
-echo $group_number
-
-echo "ruby 7_TF_DF2.rb"
-
+ruby 6_TF_DF.rb
 ruby 7_TF_DF2.rb $group_number
-
-echo "ruby 8_VSM.rb"
-
 ruby 8_VSM.rb $group_number
-
-echo "ruby 9_VSM2"
-
 ruby 9_VSM2.rb $group_number
-
-echo "./11_logistic.r"
-
 ./11_logistic.r $group_number
-
-echo "ruby 12_evaluation.rb"
-
 ruby 12_evaluation.rb $group_number
